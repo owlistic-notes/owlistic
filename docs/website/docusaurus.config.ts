4493d9fdab9cb3de0e01033acb2ff9b97c944f3c
Davide Rutigliano <davide.ruti@gmail.com>
2025-05-25 20:04:57 +0200

    Add API reference pages (generated)

--- docs/website/docusaurus.config.ts
+++ docs/website/docusaurus.config.ts
@@ -56,7 +56,7 @@ export default {
         docsPluginId: "classic",
         config: {
           owlistic: {
-            specPath: "openapi/openapi.yaml",
+            specPath: "openapi",
             outputDir: "docs/api",
             sidebarOptions: {
               groupPathsBy: "tag",
